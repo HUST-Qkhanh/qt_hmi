6034dff9e2550a7c87f3df8e246d4f761ed84bfd
AGV300 <AGV300 email>
2024-11-29 11:28:13 +0700

    db connection pool creation and handle

--- App/include/dbManager.h
+++ App/include/dbManager.h
@@ -3,12 +3,13 @@
 
 #include <bsoncxx/builder/stream/document.hpp>
 #include <bsoncxx/document/view_or_value.hpp>
-#include <mongocxx/pool.hpp>
 #include <bsoncxx/json.hpp>
 #include <iostream>
+#include <memory>
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
 #include <mongocxx/options/change_stream.hpp>
+#include <mongocxx/pool.hpp>
 #include <mongocxx/uri.hpp>
 #include <mutex>
 #include <nlohmann/json.hpp>
@@ -24,6 +25,9 @@ class MongoDBClient {
 
     // Method to initialize the MongoDB client with a custom URI (optional)
     void initialize(const std::string &uri = "mongodb://localhost:27017");
+
+    // Get a client from the pool
+    std::shared_ptr<mongocxx::client> getClient();
 
     mongocxx::database getDatabase(const std::string &dbName);
 
@@ -44,7 +48,7 @@ class MongoDBClient {
      * @param fetchedStr
      */
     void fetchAllCollection(const std::string &dbName, const std::string &collectionName, const std::string &indexKey, std::vector<std::string> &fetchedDocs);
-    
+
     /**
      * @brief Add document in existed queue
      *
@@ -54,7 +58,7 @@ class MongoDBClient {
      * @param doc
      */
     void addMidleCollection(const std::string &dbName, const std::string &collectionName, const std::string &filter, const std::string &doc);
-    
+
     void removeMidleCollection(const std::string &dbName, const std::string &collectionName, const std::string &filter);
 
     bool checkChangeStream(const std::string &dbName, const std::string &collectionName);
@@ -77,7 +81,8 @@ class MongoDBClient {
 
     mongocxx::instance instance_;  // MongoDB driver instance
     mongocxx::client dbClient_;    // MongoDB client to manage connections
-    bool isInitialized_ = false;   // Track if client has been initialized
+    std::unique_ptr<mongocxx::pool> pool_;
+    bool isInitialized_ = false;  // Track if client has been initialized
 
     // std::optional<bsoncxx::document::value> queueLatestResumeToken;
     // std::optional<bsoncxx::document::value> bufferLatestResumeToken;
